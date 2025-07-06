b715fd93ee42adacf40d1e0a0a807247b42d5d49
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-06 22:39:46 +0200

    fix: login page styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,11 +2,23 @@
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
 </script>
 
-<form method="POST" action="?/login">
-	<input type="email" name="email" required placeholder="email" />
-	<input type="password" name="password" required placeholder="password" />
-	<button type="submit">login</button>
-	<button type="submit" formaction="?/register">register</button>
-</form>
+<div class="flex h-dvh w-full items-center justify-center">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>Login / Register</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<form method="POST" action="?/login" class="flex flex-col gap-3">
+				<Input type="email" name="email" required placeholder="email" />
+				<Input type="password" name="password" required placeholder="password" />
+				<Button type="submit">login</Button>
+				<Button type="submit" formaction="?/register">register</Button>
+			</form>
+		</Card.Content>
+	</Card.Root>
+</div>
