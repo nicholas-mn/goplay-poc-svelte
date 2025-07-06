bfcd18a9945198b7628522c6e343821bad0a9d44
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-06 22:08:25 +0200

    fix: nav

--- src/lib/components/app-nav.svelte
+++ src/lib/components/app-nav.svelte
@@ -3,25 +3,30 @@
 	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
 </script>
 
-<NavigationMenu.Root>
-	<NavigationMenu.List>
-		<NavigationMenu.Item>
-			<NavigationMenu.Link href="/app" class={page.url.pathname === '/app' ? 'bg-accent' : ''}
-				>Opret</NavigationMenu.Link
-			>
-		</NavigationMenu.Item>
-		<NavigationMenu.Item>
-			<NavigationMenu.Link
-				href="/app/findleg"
-				class={page.url.pathname === '/app/findleg' ? 'bg-accent' : ''}
-				>Find leg</NavigationMenu.Link
-			>
-		</NavigationMenu.Item>
-		<NavigationMenu.Item>
-			<NavigationMenu.Link
-				href="/app/profil"
-				class={page.url.pathname === '/app/profil' ? 'bg-accent' : ''}>Profil</NavigationMenu.Link
-			>
-		</NavigationMenu.Item>
-	</NavigationMenu.List>
-</NavigationMenu.Root>
+<div class="m flex w-full justify-center rounded-2xl border py-1.5">
+	<NavigationMenu.Root>
+		<NavigationMenu.List>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link
+					href="/app"
+					class={(page.url.pathname === '/app' ? 'bg-accent' : '') +
+						' scroll-m-20 text-xl font-semibold tracking-tight'}>Opret</NavigationMenu.Link
+				>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link
+					href="/app/findleg"
+					class={(page.url.pathname === '/app/findleg' ? 'bg-accent' : '') +
+						' scroll-m-20 text-xl font-semibold tracking-tight'}>Find leg</NavigationMenu.Link
+				>
+			</NavigationMenu.Item>
+			<NavigationMenu.Item>
+				<NavigationMenu.Link
+					href="/app/profil"
+					class={(page.url.pathname === '/app/profil' ? 'bg-accent' : '') +
+						' scroll-m-20 text-xl font-semibold tracking-tight'}>Profil</NavigationMenu.Link
+				>
+			</NavigationMenu.Item>
+		</NavigationMenu.List>
+	</NavigationMenu.Root>
+</div>
