1d68b3273682eb0e410900f59949c0ccc402a539
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-06 22:44:53 +0200

    feat: temporary landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Go Play POC</h1>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
 
-<a href="/app">go to app</a>
+<div class="flex h-dvh flex-col items-center justify-center gap-10">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">GoPlay POC</h1>
+
+	<a href="/app"><Button>Go to app</Button></a>
+</div>
