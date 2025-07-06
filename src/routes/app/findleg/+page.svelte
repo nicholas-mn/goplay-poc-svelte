556b081fd79d72a25f7b4843c65e94d613e03b8e
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-07 00:11:17 +0200

    fix: map height

--- src/routes/app/findleg/+page.svelte
+++ src/routes/app/findleg/+page.svelte
@@ -5,7 +5,9 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
-<div class="h-[95%]">
-	Find Leg
-	<Map />
+<div class="flex h-full flex-col">
+	<h1>Find leg</h1>
+	<div class="h-[100%]">
+		<Map />
+	</div>
 </div>
