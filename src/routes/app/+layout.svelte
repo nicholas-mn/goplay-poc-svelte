c1929b09a1d7f7515adf8b202dd9711fb26323e3
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-06 22:24:35 +0200

    feat: make a web app non-zoomable on mobile devices

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -6,6 +6,13 @@
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 </script>
 
+<svelte:head>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+	/>
+</svelte:head>
+
 <div class="mx-auto flex h-dvh max-w-lg flex-col justify-between gap-2 p-2.5">
 	<main class="h-full rounded-2xl border p-2.5">
 		{@render children()}
