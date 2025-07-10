3fd671d511816b96dae5a3328382e5697a57be3a
nicholas-mn <9296369+nicholas-mn@users.noreply.github.com>
2025-07-10 22:31:50 +0200

    temp: testing functionality for geojson and finding play dates

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,5 +1,72 @@
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
-    return { user: locals.user };
+	const data = {
+		type: 'FeatureCollection',
+		features: [
+			{
+				type: 'Feature',
+				properties: {
+					id: 1,
+					name: 'Location 1',
+					description: 'Random point in Denmark #1'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [10.237654, 56.123456]
+				}
+			},
+			{
+				type: 'Feature',
+				properties: {
+					id: 2,
+					name: 'Location 2',
+					description: 'Random point in Denmark #2'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [9.562341, 55.789012]
+				}
+			},
+			{
+				type: 'Feature',
+				properties: {
+					id: 3,
+					name: 'Location 3',
+					description: 'Random point in Denmark #3'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [12.345678, 55.456789]
+				}
+			},
+			{
+				type: 'Feature',
+				properties: {
+					id: 4,
+					name: 'Location 4',
+					description: 'Random point in Denmark #4'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [11.876543, 56.654321]
+				}
+			},
+			{
+				type: 'Feature',
+				properties: {
+					id: 5,
+					name: 'Location 5',
+					description: 'Random point in Denmark #5'
+				},
+				geometry: {
+					type: 'Point',
+					coordinates: [8.765432, 54.987654]
+				}
+			}
+		]
+	};
+
+	return { geojson: data };
 }) satisfies PageServerLoad;
+
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,13 +1,95 @@
 <script lang="ts">
-	import Map from '$lib/components/map.svelte';
 	import type { PageData } from './$types';
-
 	let { data }: { data: PageData } = $props();
+
+	import maplibregl from 'maplibre-gl';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import { onMount } from 'svelte';
+
+	// Define the map syle (OpenStreetMap raster tiles)
+	const style: maplibregl.StyleSpecification = {
+		version: 8,
+		sources: {
+			osm: {
+				type: 'raster',
+				tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+				tileSize: 256,
+				attribution: '&copy; OpenStreetMap Contributors',
+				maxzoom: 19
+			}
+		},
+		layers: [
+			{
+				id: 'osm',
+				type: 'raster',
+				source: 'osm' // This must match the source key above
+			}
+		]
+	};
+
+	onMount(() => {
+		// Initialise the map
+		const map = new maplibregl.Map({
+			container: 'map',
+			style: style,
+			center: [10, 56],
+			zoom: 5
+		});
+
+		// Add the navigation control
+		map.addControl(new maplibregl.NavigationControl());
+
+		let geolocate = new maplibregl.GeolocateControl({
+			positionOptions: {
+				enableHighAccuracy: true
+			},
+			trackUserLocation: true
+		});
+		// Add the control to the map.
+		map.addControl(geolocate);
+
+		map.on('load', async () => {
+			const image = await map.loadImage('/marker.png');
+			map.addImage('marker', image.data);
+			map.addSource('lege', {
+				type: 'geojson',
+				data: data.geojson as any
+			});
+
+			map.addLayer({
+				id: 'lege',
+				type: 'symbol',
+				source: 'lege',
+				layout: {
+					'icon-image': 'marker',
+					'icon-size': 0.05
+				}
+			});
+
+			map.on('click', 'lege', (e: any) => {
+				map.flyTo({
+					center: e.features[0].geometry.coordinates
+				});
+			});
+
+			// Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
+			map.on('mouseenter', 'lege', () => {
+				map.getCanvas().style.cursor = 'pointer';
+			});
+
+			// Change it back to a pointer when it leaves.
+			map.on('mouseleave', 'lege', () => {
+				map.getCanvas().style.cursor = '';
+			});
+
+			geolocate.trigger();
+		});
+	});
 </script>
 
 <div class="flex h-full flex-col">
 	<h1>Find leg</h1>
 	<div class="h-[100%]">
-		<Map />
+		<div id="map" class="h-full rounded-2xl"></div>
 	</div>
 </div>
